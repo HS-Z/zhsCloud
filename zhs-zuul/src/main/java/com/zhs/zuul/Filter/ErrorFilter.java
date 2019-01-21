28f79fa357e14a5b20544a5f2afb3dcd3bee3bd6
HS-Z <www.aiwoo@live.com>
2019-01-21 13:59:01 +0800

    error

--- zhs-zuul/src/main/java/com/zhs/zuul/Filter/ErrorFilter.java
+++ zhs-zuul/src/main/java/com/zhs/zuul/Filter/ErrorFilter.java
@@ -1,7 +1,49 @@
 package com.zhs.zuul.Filter;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Created by Zhang on 2019/1/21.
  */
-public class ErrorFilter {
+public class ErrorFilter extends ZuulFilter{
+
+    private static Logger log = LoggerFactory.getLogger(ErrorFilter.class);
+
+
+    @Override
+    public String filterType() {
+        //异常处理器
+        return "error";
+    }
+
+    @Override
+    public int filterOrder() {
+        //优先级，数字越小，越先执行
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        //是否执行该过滤器，true代表需要过滤
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+
+        log.info("进入异常过滤器");
+
+        System.out.println(ctx.getResponseBody());
+
+        ctx.setResponseBody("出现异常");
+
+        return null;
+    }
 }
