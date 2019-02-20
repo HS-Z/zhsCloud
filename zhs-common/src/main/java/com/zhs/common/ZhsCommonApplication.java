8e4127ec57f7d31ea954caf45d4cb1206e666e04
HS-Z <www.aiwoo@live.com>
2019-02-20 14:26:13 +0800

    common

--- zhs-common/src/main/java/com/zhs/common/ZhsCommonApplication.java
+++ zhs-common/src/main/java/com/zhs/common/ZhsCommonApplication.java
@@ -4,12 +4,10 @@ package com.zhs.common;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication(scanBasePackages = {"com.zhs"})
 @EnableRedisHttpSession   //开启session管理
-@EnableTransactionManagement  //开启事务管理
 @EnableEurekaClient
 public class ZhsCommonApplication {
 
