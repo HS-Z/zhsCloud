d6cda64b95c698a173acc09e2740a21351eb59f7
HS-Z <www.aiwoo@live.com>
2019-03-28 17:33:55 +0800

    尴尬

--- zhs-common/src/main/java/com/zhs/common/rabbitMQ/RabbitSend.java
+++ zhs-common/src/main/java/com/zhs/common/rabbitMQ/RabbitSend.java
@@ -1,20 +1,31 @@
 package com.zhs.common.rabbitMQ;
 
 import com.zhs.common.constant.RabbitMQ;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * Created by Zhang on 2019/3/20.
  * 消息生产者
  */
 @Component
-public class RabbitSend {
+public class RabbitSend implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnCallback {
+
+
+    private RabbitTemplate rabbitTemplate;
 
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    public RabbitSend(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+        rabbitTemplate.setConfirmCallback(this);
+    }
 
 
     public void send(String a, Object o){
@@ -31,4 +42,40 @@ public class RabbitSend {
     }
 
 
+    public void send2(){
+        for (int i = 0; i < 1000; i++) {
+            String context = "hi, i am messages " + i;
+            System.out.println("Sender : " + context);
+            CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+            System.out.println("callbackSender UUID: " + correlationData.getId());
+            this.rabbitTemplate.convertAndSend("topicExchange", "topic.messages", context, correlationData);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * 回调
+     * @param correlationData
+     * @param b
+     * @param s
+     */
+    @Override
+    public void confirm(@Nullable CorrelationData correlationData, boolean b, @Nullable String s) {
+        if (b) {
+            System.out.println("消息发送成功:" + correlationData);
+        } else {
+            System.out.println("消息发送失败:" + s);
+        }
+    }
+
+
+    @Override
+    public void returnedMessage(Message message, int i, String s, String s1, String s2) {
+        System.out.println(message.getMessageProperties() + " 发送失败");
+    }
 }
